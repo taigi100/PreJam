3d1871f4da1a28454273cfdfcd2ede624be78fbf
taigi100 <contact@robertcazacu.com>
2025-01-23 01:19:49 +0200

    Fix movement & refactor

--- Project/cam_movement.gd
+++ Project/cam_movement.gd
@@ -1,32 +1,34 @@
 extends Node3D
 
-var mouse_cam_sensitivity = 0.005
+@export var mouse_sensitivity = 0.005
+@export var player: NodePath  # Assign the player node in the Inspector
+
+@onready var twist_pivot = $h as Node3D
+@onready var pitch_pivot = $h/v as Node3D
+
+@export var min_tilt = deg_to_rad(-45)  # Looking straight down
+@export var max_tilt = deg_to_rad(-40)   # Looking straight up
+
 var twist_input = 0.0
 var pitch_input = 0.0
-var input_dir
-
-@onready var twitst_pivot = %h as Node3D
-@onready var pitch_pivot = %v as Node3D
-
-@export var min_tilt = deg_to_rad(-45)  # More horizontal view
-@export var max_tilt = deg_to_rad(-40)  # Nearly top-down view
 
 func _physics_process(delta: float) -> void:
-	twitst_pivot.rotate_y(twist_input)
+	if player:
+		position = get_node(player).position  # Update camera position to match player
+
+	# Apply rotation inputs
+	twist_pivot.rotate_y(twist_input)
 	pitch_pivot.rotate_x(pitch_input)
 	
-	pitch_pivot.rotation.x = clamp(
-		pitch_pivot.rotation.x,
-		min_tilt,
-		max_tilt
-	)
-	print(rad_to_deg(pitch_pivot.rotation.x))
+	# Clamp pitch rotation
+	pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, min_tilt, max_tilt)
 	
+	# Reset inputs
 	twist_input = 0.0
 	pitch_input = 0.0
-	
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-			twist_input = -event.relative.x * mouse_cam_sensitivity
-			pitch_input = -event.relative.y * mouse_cam_sensitivity
+			twist_input = -event.relative.x * mouse_sensitivity
+			pitch_input = -event.relative.y * mouse_sensitivity
--- Project/character.gd
+++ Project/character.gd
@@ -1,72 +1,62 @@
 extends CharacterBody3D
 
+@export var speed = 5.0
+@export var jump_velocity = 4.5
+@export var turn_speed = 30.0
+@export var camera_pivot: NodePath  # Assign the camera pivot node in the Inspector
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-const TURN_SPEED = 30
-
-@onready var mesh := $MeshInstance3D # Add reference to your mesh instance
-
-var parent
-
-var bullet = load("res://enemy/bullet.tscn")
-var instance
-
-@onready var cam_h = %h as Node3D
-@onready var cam_v = %v as Node3D
 @onready var gun_anim = $Weapon/AnimationPlayer
 @onready var gun_barrel = $Weapon/RayCast3D
 
-@export_range(0.0, 1.0) var mouse_sensitivity = 0.01
+var bullet = load("res://enemy/bullet.tscn")
+var can_shoot = true
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	parent = get_parent()
-	
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("exit"): 
 		get_tree().quit()
-		
-func _process(delta: float) -> void:
-	parent.position = position
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Add gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += get_gravity().y * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get input direction.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
-	# Transform input direction based on character's rotation
-	var move_dir = Vector3.ZERO
-	move_dir.x = input_dir.x
-	move_dir.z = input_dir.y  # Negative because forward is -Z in Godot
-	move_dir = move_dir.rotated(Vector3.UP, rotation.y)
-	
-	if move_dir:
-		velocity.x = move_dir.x * SPEED
-		velocity.z = move_dir.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	var move_dir = Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, rotation.y)
 
-	# Rotate the character to match camera's horizontal rotation
-	var target_rotation = cam_h.rotation.y
-	rotation.y = lerp_angle(rotation.y, target_rotation, TURN_SPEED * delta)
-	
-	if Input.is_action_pressed("attack"):
-		if !gun_anim.is_playing():
-			gun_anim.play("shoot")
-			instance = bullet.instantiate()
-			instance.position = gun_barrel.global_position
-			instance.transform.basis = gun_barrel.global_transform.basis
-			get_parent().add_child(instance)
-		
-	
+	# Apply movement.
+	if move_dir:
+		velocity.x = move_dir.x * speed
+		velocity.z = move_dir.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+	# Rotate the character to match camera's horizontal rotation.
+	if camera_pivot:
+		var cam_h = get_node(camera_pivot).get_node("h")
+		rotation.y = lerp_angle(rotation.y, cam_h.rotation.y, turn_speed * delta)
+
+	# Handle shooting.
+	if Input.is_action_pressed("attack") and can_shoot:
+		shoot()
+
 	move_and_slide()
+
+func shoot():
+	if !gun_anim.is_playing():
+		gun_anim.play("shoot")
+		var instance = bullet.instantiate()
+		instance.position = gun_barrel.global_position
+		instance.transform.basis = gun_barrel.global_transform.basis
+		get_parent().add_child(instance)
+		can_shoot = false
+		await get_tree().create_timer(0.5).timeout  # Cooldown
+		can_shoot = true
